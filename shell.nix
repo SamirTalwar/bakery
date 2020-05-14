d9804dc47d1b3683e904c4c428c415d4f940ca24
Samir Talwar <samir@noodlesandwich.com>
2020-05-14 16:10:08 +0200

    Update Smoke to the latest version.

--- shell.nix
+++ shell.nix
@@ -1,10 +1,10 @@
 { pkgs ? import <nixpkgs> {}
-, smoke ? import (pkgs.fetchFromGitHub {
+, smoke ? (import (pkgs.fetchFromGitHub {
     owner = "SamirTalwar";
     repo = "smoke";
-    rev = "eec4dd710a3fff495c268f6c7c606f21ab625ed6";
-    sha256 = "01qsnicj12f9nv07fd75v3n4xmj4dzgcb3fv2700q3583zxxnkk6";
-  }) {}
+    rev = "944ec824a5906b6bf910289a4e1c382cdd6e8324";
+    sha256 = "1rv7q2i086i1k5kfscm97w4zlg04i60ws5z1pm8q907grydp2sgf";
+  }) {}).smoke
 }:
 
 with pkgs;
