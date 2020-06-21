97eb1c14ea4f977a9c482faebd8e86e817168dcb
Samir Talwar <samir@noodlesandwich.com>
2020-06-21 15:28:46 +0200

    Switch to `nom` as the parser combinator library.

--- src/parsers.rs
+++ src/parsers.rs
@@ -1,41 +1,7 @@
-use std::error;
-use std::fmt;
+use nom::character::complete::{line_ending, not_line_ending};
 
-pub type Input<'a> = &'a str;
-
-pub type Parsed<'a, T> = Result<(Input<'a>, T), Error<'a>>;
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub enum Error<'a> {
-    Rejected(&'a str),
-}
-
-impl<'a> fmt::Display for Error<'a> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
-        match self {
-            Self::Rejected(remainder) => write!(f, "Rejected: {}", remainder),
-        }
-    }
-}
-
-impl<'a> error::Error for Error<'a> {}
-
-pub fn match_literal<'literal: 'a, 'a>(
-    expected: &'literal str,
-) -> impl Fn(Input<'a>) -> Parsed<'_, ()> {
-    move |input| match input.get(0..expected.len()) {
-        Some(next) if next == expected => Ok((&input[expected.len()..], ())),
-        _ => Err(Error::Rejected(input)),
-    }
-}
-
-pub fn comment<'a>(input: Input<'a>) -> Parsed<'_, ()> {
-    let (text, _) = match_literal("#")(input)?;
-    match text.find("\n") {
-        Some(newline) => Ok((&text[newline + 1..], ())),
-        None => Ok(("", ())),
-    }
-}
+named!(comment<&str, &str>,
+       terminated!(preceded!(tag!("#"), not_line_ending), opt!(line_ending)));
 
 #[cfg(test)]
 mod tests {
@@ -45,53 +11,24 @@ mod tests {
 
     proptest! {
         #[test]
-        fn match_literal_consumes(expected in any::<String>()) {
-            let string = expected.clone();
-            let parsed = match_literal(&expected)(&string)?;
-            prop_assert_eq!(("", ()), parsed);
-        }
-
-        #[test]
-        fn match_literal_can_be_chained(expected_a in any::<String>(), expected_b in any::<String>(), expected_c in any::<String>()) {
-            let string = expected_a.clone() + &expected_b + &expected_c;
-
-            let parsed_a = match_literal(&expected_a)(&string)?;
-            let expected_remainder_a: String = expected_b.clone() + &expected_c;
-            prop_assert_eq!((expected_remainder_a.as_str(), ()), parsed_a);
-
-            let parsed_b = match_literal(&expected_b)(parsed_a.0)?;
-            prop_assert_eq!((expected_c.as_str(), ()), parsed_b);
-
-            let parsed_c = match_literal(&expected_c)(parsed_b.0)?;
-            prop_assert_eq!(("", ()), parsed_c);
-        }
-
-        #[test]
-        fn match_literal_rejects_any_other_input(expected in any::<String>(), input in any::<String>()) {
-            prop_assume!(!input.starts_with(&expected));
-
-            let result = match_literal(&expected)(&input);
-            prop_assert_eq!(Err(Error::Rejected(&input)), result);
-        }
-
-        #[test]
-        fn comment_matches_a_comment_marker_to_the_end_of_the_line(comment_text in any::<String>()) {
-            let input = "#".to_string() + &comment_text + "\n";
+        fn comment_matches_a_comment_marker_to_the_end_of_the_line(comment_text in "#\\PC*", next_line in any::<String>()) {
+            let expected = comment_text[1..].to_string();
+            let input = comment_text.to_string() + "\n" + &next_line;
             let parsed = comment(&input)?;
-            prop_assert_eq!(("", ()), parsed);
+            prop_assert_eq!((next_line.as_str(), expected.as_str()), parsed);
         }
 
         #[test]
-        fn comment_matches_a_comment_marker_on_the_last_line(comment_text in any::<String>()) {
-            let input = "#".to_string() + &comment_text;
+        fn comment_matches_a_comment_marker_on_the_last_line(input in "#\\PC*") {
+            let expected = input[1..].to_string();
             let parsed = comment(&input)?;
-            prop_assert_eq!(("", ()), parsed);
+            prop_assert_eq!(("", expected.as_str()), parsed);
         }
 
         #[test]
         fn comment_does_not_match_anything_else(input in "[^#]\\PC*") {
             let parsed = comment(&input);
-            prop_assert_eq!(Err(Error::Rejected(&input)), parsed);
+            prop_assert!(parsed.is_err());
         }
     }
 }
