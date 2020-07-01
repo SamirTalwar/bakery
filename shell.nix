e472f756a561b9510c4901289d13cf7bd0974f4b
Samir Talwar <samir@noodlesandwich.com>
2020-07-01 19:14:08 +0200

    Get Rust Racer working from nixos-20.03.
    The version from nixpkgs-unstable doesn't build right now.

--- shell.nix
+++ shell.nix
@@ -1,18 +1,19 @@
 let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs {};
+  stablePkgs = import sources."nixos-20.03" {};
   rust = import ./nix/rust.nix;
   smoke = (import sources.smoke {}).smoke;
 in
-with pkgs;
-mkShell {
+pkgs.mkShell {
   name = "bakery";
 
   buildInputs = [
     rust.cargo
     rust.rustc
     rust.rustfmt
+    stablePkgs.rustracer
     smoke
-    niv
-  ] ++ (if stdenv.isDarwin then [darwin.apple_sdk.frameworks.Security] else []);
+    pkgs.niv
+  ] ++ (if pkgs.stdenv.isDarwin then [pkgs.darwin.apple_sdk.frameworks.Security] else []);
 }
