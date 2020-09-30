46b513f526b57cd2bc506802ad6e28aa512c6128
Samir Talwar <samir@noodlesandwich.com>
2020-10-01 00:15:05 +0200

    Pipe the data from the source directly into the sink.

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,16 +1,16 @@
+use std::io;
+
 use super::ast::Expression;
 use super::errors;
 use super::errors::{Error, Result};
 
 pub fn interpret(expression: Expression) -> Result<()> {
-    let mut buffer: Vec<u8> = Vec::new();
     match expression {
         Expression::Pipe { source, sink } => match (*source, *sink) {
             (Expression::Block(source_block), Expression::Block(sink_block)) => {
-                let mut open_source = source_block.source()?;
-                open_source.read_to_end(&mut buffer).map_err(errors::io)?;
-                let mut open_sink = sink_block.sink()?;
-                open_sink.write_all(&buffer[..]).map_err(errors::io)?;
+                let mut open_source = source_block.source().map(io::BufReader::new)?;
+                let mut open_sink = sink_block.sink().map(io::BufWriter::new)?;
+                io::copy(&mut open_source, &mut open_sink).map_err(errors::io)?;
                 Ok(())
             }
             _ => Err(Error::UninterpretableProgram),
