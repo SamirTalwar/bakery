4c6f3559131fb89db475a9adff75694b062e919f
Samir Talwar <samir@noodlesandwich.com>
2022-08-05 23:22:19 +0200

    Run the examples with Nix.

--- flake.nix
+++ flake.nix
@@ -23,10 +23,19 @@
       pkgs = import nixpkgs { inherit system; };
       haskellCompiler = pkgs.haskell.compiler."${ghcVersionString}";
       haskellPackages = pkgs.haskell.packages."${ghcVersionString}";
+
+      examples = builtins.attrNames (pkgs.lib.attrsets.filterAttrs (name: value: value == "directory") (builtins.readDir ./examples));
     in
     rec {
       packages.lib = haskellPackages.callCabal2nix name ./lib { };
+      packages.examples = haskellPackages.callCabal2nix "${name}-examples" ./examples { bakery = packages.lib; };
       packages.default = packages.lib;
+
+      # Lets you run the examples with Nix.
+      # For example, `nix run .#examples.echo` will run the "echo" example.
+      apps.examples = pkgs.lib.attrsets.genAttrs examples (name:
+        flake-utils.lib.mkApp { drv = packages.examples; exePath = "/bin/${name}"; }
+      );
 
       formatter = pkgs.nixpkgs-fmt;
 
