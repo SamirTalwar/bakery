1aba3376fc631517d9302e75b207a80a5c5d07d8
Samir Talwar <samir@noodlesandwich.com>
2022-08-13 13:35:31 +0200

    Reduce indentation in Bake.hs.

--- lib/src/Bakery/Bake.hs
+++ lib/src/Bakery/Bake.hs
@@ -25,6 +25,13 @@ import System.Directory qualified as Directory
 import System.Environment (getArgs, lookupEnv)
 import System.IO (hPutStrLn, stderr)
 
+bakeable :: Namespace -> Maybe (Is Bakeable)
+bakeable (Namespace "file") = Just $ Is @Bakery.File.File
+bakeable _ = Nothing
+
+recipe :: forall a. Bakeable a => a -> Recipe a -> BakeT Baking a
+recipe = defineRecipe
+
 bake :: BakeT Baking a -> IO ()
 bake thing = do
   args <- map Text.pack <$> getArgs
@@ -49,61 +56,24 @@ actuallyBake thing args = do
       targets <- mapM parseTarget args
       targetOutputs <- mapM (findTarget outputs) targets
       bakeOutputs outputs targetOutputs
+
+-- Yes, this parser is currently a joke. We shall fix that.
+parseTarget :: Text -> Baking Id
+parseTarget text =
+  case Text.findIndex (== ':') text of
+    Nothing -> identifier <$> (maybe reject pure =<< parseName @Bakery.File.File text)
+    Just separatorIndex ->
+      let (targetNamespace, targetName) = bimap Namespace (Text.drop 1) $ Text.splitAt separatorIndex text
+       in case bakeable targetNamespace of
+            Nothing -> reject
+            Just isBakeable -> parseName' isBakeable targetName
   where
-    -- Yes, this parser is currently a joke. We shall fix that.
-    parseTarget :: Text -> Baking Id
-    parseTarget text =
-      case Text.findIndex (== ':') text of
-        Nothing -> identifier <$> (maybe reject pure =<< parseName @Bakery.File.File text)
-        Just separatorIndex ->
-          let (targetNamespace, targetName) = bimap Namespace (Text.drop 1) $ Text.splitAt separatorIndex text
-           in case bakeable targetNamespace of
-                Nothing -> reject
-                Just isBakeable -> parseName' isBakeable targetName
-      where
-        reject :: Baking a
-        reject = fail $ "I don't know what " <> show text <> " is."
-
-    findTarget :: Outputs -> Id -> Baking (An Output)
-    findTarget outputs target =
-      let targetOutput = List.find (\(An Output {outputId}) -> outputId == target) outputs
-       in maybe (fail $ "Cannot bake " <> show target) pure targetOutput
-
-    bakeOutputs :: Outputs -> Outputs -> Baking ()
-    bakeOutputs allOutputs targetOutputs = do
-      requiredOutputs <- required allOutputs targetOutputs
-      logText "Plan:"
-      forM_ requiredOutputs (\(An Output {outputId}) -> logValue outputId)
-      logText ""
-      mapM_ bakeOutput requiredOutputs
-
-    bakeOutput :: An Output -> Baking ()
-    bakeOutput (An output@Output {outputAction, outputExists}) = do
-      logText ("Baking " <> show output <> "...")
-      void outputAction
-      doesExist <- outputExists
-      unless doesExist . fail $ "Did not produce " <> show output <> "."
-
-    required :: Outputs -> Outputs -> Baking Outputs
-    required allOutputs targetOutputs = do
-      requiredTargets <- forM targetOutputs $ \targetOutput@(An Output {outputInputs}) -> do
-        dependencies <- mapM (\(An (Input input)) -> findTarget allOutputs (identifier input)) outputInputs
-        pure $ dependencies ++ [targetOutput]
-      pure . List.nubBy equalById $ concat requiredTargets
-      where
-        equalById = (==) `on` (\(An Output {outputId}) -> outputId)
-
-recipe :: forall a. Bakeable a => a -> Recipe a -> BakeT Baking a
-recipe = defineRecipe
-
-bakeable :: Namespace -> Maybe (Is Bakeable)
-bakeable (Namespace "file") = Just $ Is @Bakery.File.File
-bakeable _ = Nothing
+    reject :: Baking a
+    reject = fail $ "I don't know what " <> show text <> " is."
 
 -- HLint incorrectly tries to remove brackets around '(Is @b)'.
-{- HLINT ignore parseName' "Redundant bracket" -}
-
 -- Ormolu doesn't know how to deal with type applications in constructors yet.
+{- HLINT ignore parseName' "Redundant bracket" -}
 parseName' :: Is Bakeable -> Text -> Baking Id
 {- ORMOLU_DISABLE -}
 parseName' (Is @a) targetName = identifier <$> (maybe reject pure =<< parseName @a targetName)
@@ -112,7 +82,36 @@ parseName' (Is @a) targetName = identifier <$> (maybe reject pure =<< parseName 
     reject = fail $ show targetName <> " cannot be parsed in the namespace \"" <> namespaceString <> "\"."
 {- ORMOLU_ENABLE -}
 
-{-# ANN logText ("HLint: ignore Avoid lambda using `infix`" :: String) #-}
+findTarget :: Outputs -> Id -> Baking (An Output)
+findTarget outputs target =
+  let targetOutput = List.find (\(An Output {outputId}) -> outputId == target) outputs
+   in maybe (fail $ "Cannot bake " <> show target) pure targetOutput
+
+bakeOutputs :: Outputs -> Outputs -> Baking ()
+bakeOutputs allOutputs targetOutputs = do
+  requiredOutputs <- required allOutputs targetOutputs
+  logText "Plan:"
+  forM_ requiredOutputs (\(An Output {outputId}) -> logValue outputId)
+  logText ""
+  mapM_ bakeOutput requiredOutputs
+
+bakeOutput :: An Output -> Baking ()
+bakeOutput (An output@Output {outputAction, outputExists}) = do
+  logText ("Baking " <> show output <> "...")
+  void outputAction
+  doesExist <- outputExists
+  unless doesExist . fail $ "Did not produce " <> show output <> "."
+
+required :: Outputs -> Outputs -> Baking Outputs
+required allOutputs targetOutputs = do
+  requiredTargets <- forM targetOutputs $ \targetOutput@(An Output {outputInputs}) -> do
+    dependencies <- mapM (\(An (Input input)) -> findTarget allOutputs (identifier input)) outputInputs
+    pure $ dependencies ++ [targetOutput]
+  pure . List.nubBy equalById $ concat requiredTargets
+  where
+    equalById = (==) `on` (\(An Output {outputId}) -> outputId)
+
+{- HLINT ignore logText "Avoid lambda using `infix`" -}
 logText :: String -> Baking ()
 logText text = Baking do
   handle <- Reader.asks logger
