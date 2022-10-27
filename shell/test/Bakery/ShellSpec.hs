66f8876f35482f4652b9143f126843fff9d74a92
Samir Talwar <samir@noodlesandwich.com>
2022-10-27 23:52:56 +0200

    shell: Ensure that errors from a pipe are propagated.

--- shell/test/Bakery/ShellSpec.hs
+++ shell/test/Bakery/ShellSpec.hs
@@ -1,7 +1,10 @@
 module Bakery.ShellSpec (spec) where
 
 import Bakery.Shell
+import Bakery.Shell.Operation
 import Bakery.Shell.Prelude qualified as B
+import Pipes qualified
+import System.IO.Error (tryIOError)
 import Test.Hspec
 
 spec :: Spec
@@ -13,3 +16,13 @@ spec = do
           operation = B.filter even |> B.map (* 2)
       result <- evaluate operation values
       result `shouldBe` [4, 8 .. 40]
+
+    it "propagates errors" do
+      let operation :: () #> Int
+          operation = Operation [] do
+            Pipes.yield 1
+            Pipes.yield 2
+            Pipes.yield 3
+            fail "Oh no!"
+      result <- tryIOError $ evaluate operation []
+      result `shouldBe` Left (userError "Oh no!")
