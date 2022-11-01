535af1f8457dae4dc3f5b19960bb841e2568a6b5
Samir Talwar <samir@noodlesandwich.com>
2022-11-01 21:56:54 +0100

    shell: Add a test for tracking inputs.

--- shell/test/Bakery/ShellSpec.hs
+++ shell/test/Bakery/ShellSpec.hs
@@ -1,8 +1,14 @@
 module Bakery.ShellSpec (spec) where
 
+import Bakery.A
+import Bakery.Identifier
+import Bakery.Input
 import Bakery.Shell
+import Bakery.Shell.Argument
 import Bakery.Shell.Prelude qualified as B
 import Control.Monad.Trans (lift)
+import Data.Text qualified as Text
+import Data.Void (Void)
 import Pipes qualified
 import System.IO.Error (tryIOError)
 import Test.Hspec
@@ -37,3 +43,30 @@ spec = do
             operationB -- works on the remainder
       result <- evaluate operation values
       result `shouldBe` [4, 8 .. 20] <> [33, 39 .. 57]
+
+  describe "input tracking" do
+    it "tracks any inputs referenced" do
+      let operation :: () #> Void
+          operation = run_ (TrackedArg "./one") (TrackedArg "./two")
+          inputs = getInputs operation
+      case inputs of
+        [An inputA, An inputB] -> do
+          -- abusing `show` to check whether the arguments are legitimate
+          show inputA `shouldBe` show (Input (TrackedArg "./one"))
+          show inputB `shouldBe` show (Input (TrackedArg "./two"))
+        _ -> fail ("Unexpected inputs: " <> show inputs)
+
+newtype TrackedArg a = TrackedArg a
+
+instance Show a => Show (TrackedArg a) where
+  show (TrackedArg x) = show x
+
+instance Show a => Identifiable (TrackedArg a) where
+  namespace _ = Namespace (Text.pack "tracked arg")
+  name x = Name (Text.pack (show x))
+
+instance Argument (TrackedArg String) where
+  toArg (TrackedArg x) = StringArg x
+
+instance Show a => HasInputs (TrackedArg a) where
+  getInputs x = [An (Input x)]
